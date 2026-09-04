987b47acff0c5154058ee1feba5ad197cb47edd0
krulikovskiy <krulikovskiy@bruenok.ru>
2026-09-04 16:31:18 +0300

    Revert "feat(world): preview block city with production terrain"
    This reverts commit 33853e03797b0eebad643b802b8c5ea0346be586.

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -7,14 +7,8 @@ import "./styles.css";
 document.documentElement.dataset.appVersion = __TASKTOPIA_VERSION__;
 registerTasktopiaServiceWorker();
 
-const blockWorldPreview = import.meta.env.VITE_BLOCK_WORLD_PREVIEW === "1"
-  && new URLSearchParams(window.location.search).get("block-preview") === "1";
-const root = createRoot(document.getElementById("root")!);
-
-if (blockWorldPreview) {
-  void import("./components/BlockWorldPreview").then(({ BlockWorldPreview }) => {
-    root.render(<StrictMode><BlockWorldPreview /></StrictMode>);
-  });
-} else {
-  root.render(<StrictMode><App /></StrictMode>);
-}
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
